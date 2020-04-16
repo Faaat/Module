a281b1ca82fbed20a62cc1b77f92ed8251f03f5a
方鑫 <15164609463@163.com>
2020-04-16 23:29:41 +0800

    增加字符串对比，支持忽略大小写，输入格式仅支持数字、小写字母、大写字母

--- baselibrary/src/main/java/com/felix/baselibrary/Utils/StringUtils.java
+++ baselibrary/src/main/java/com/felix/baselibrary/Utils/StringUtils.java
@@ -22,4 +22,33 @@ public class StringUtils {
     public boolean isEmpty(String s) {
         return s.equals("") || TextUtils.isEmpty(s);
     }
+
+    /**
+     * @param s1              字符串
+     * @param s2              字符串
+     * @param isIgnoreCapital 是否忽略大写
+     * @return 相等:true;不相等:false
+     */
+    public boolean compareText(String s1, String s2, boolean isIgnoreCapital) {
+        if (!isIgnoreCapital) {
+            return s1.equals(s2);
+        }
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            char pswChar = s1.charAt(i);
+            char checkChar = s2.charAt(i);
+            if (pswChar != checkChar) {
+                if ((pswChar >= 'a' && pswChar <= 'z') || (pswChar >= 'A' && pswChar <= 'Z')) {
+                    if (pswChar + 32 != checkChar && pswChar - 32 != checkChar) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
